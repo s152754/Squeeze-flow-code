8df3b7e36c3d3a9abbadf9181b0e4c313a9fb4ad
s152754 <a.rinkens@student.tue.nl>
2022-06-13 18:18:18 +0200

    addf forward file

---
+++ ForwardUQ.m
@@ -0,0 +1,20 @@
+clear all; close all; clc;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Title: Uncertainty quantification                                   %%%
+%%% Author: Aricia Rinkens                                              %%%
+%%% Description: UQ to compare between model(s) and to experiments.     %%%   
+%%% Output:                                                             %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Input files
+% experiment
+% hier laad je een file in waarbij de kolommen het aantal experimenten is +
+% een gemiddelde + standard deviatie en tijd
+
+% model
+
+% parametric uncertainty
+
+%% Model comparison
+
+%% Forward Uncertainty Quantification
